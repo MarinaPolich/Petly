76bbdea9f02a6ba5125de22810a2fcd93862cfc1
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-01-30 23:09:00 +0200

    Add redux notices operations, selectors, slice (getNotices, addNotices)

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,7 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { authReducer } from "./auth/auth-slice";
+import { noticesReducer } from "./notices/notices-slice";
 
 const middleware = [
   ...getDefaultMiddleware({
@@ -29,6 +30,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
+    notices: noticesReducer,
   },
   middleware,
   // devTools: process.env.NODE_ENV === 'development',
