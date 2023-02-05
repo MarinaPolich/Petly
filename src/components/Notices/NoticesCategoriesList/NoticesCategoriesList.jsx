c0c5465c68ff456e82e0f313f0f84391650e44e6
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-02-06 01:27:43 +0200

    Update CategoriesList (add category)

--- src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,17 +1,20 @@
-import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { getNoticesByCategories } from "redux/notices/notices-operations";
 import { noticesSelector } from "redux/notices/notices-selector";
 import NoticeCategoryItem from "../NoticeCategoryItem/NoticeCategoryItem";
 import { BoxList } from "./NoticesCategoriesList.styled";
 
 const NoticesCategoriesList = () => {
+  const { category } = useParams();
   const dispatch = useDispatch();
 
   const notices = useSelector(noticesSelector);
+  console.log(notices);
 
   useEffect(() => {
-    dispatch(getNoticesByCategories());
+    dispatch(getNoticesByCategories(category));
   }, [dispatch]);
 
   return (
