20afc264b7e5c924dc15bb01dcda6e85f5d38ce8
Marina <35706436+MarinaPolich@users.noreply.github.com>
2023-02-05 16:46:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { GlobalStyle } from "./index.styled.js";
 import "stylesheet/vars.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+ <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
@@ -18,4 +19,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </BrowserRouter>
     </PersistGate>
   </Provider>
+   </React.StrictMode>
 );
