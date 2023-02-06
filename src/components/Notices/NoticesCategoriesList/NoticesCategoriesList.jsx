d9d8418a4ee96d0b19b47964dc8cc4468974a031
ArturMandryk <ArturMandryk@gmail.com>
2023-02-06 21:08:49 +0200

    Update NoticesCategoriesList.jsx

--- src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -17,7 +17,6 @@ const NoticesCategoriesList = () => {
   const filteredNotices = useSelector(filteredNoticesSelector);
 
   useEffect(() => {
-    dispatch(getNoticesByCategories({ category }));
     dispatch(
       getNoticesByCategories(
         category === "for-free" ? "in-good-hands" : { category }
