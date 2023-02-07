59c49003c8e361ffcfbeef2ee77709722531bf5c
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-02-07 17:11:17 +0200

    Update pagination

--- src/components/Notices/NoticesCategoriesList/NoticesOwnList.jsx
+++ src/components/Notices/NoticesCategoriesList/NoticesOwnList.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userNotice } from "redux/notices/notices-operations";
-// import { getUser } from "redux/auth/auth-selector";
 import { filteredNoticesSelector } from "redux/notices/notices-selector";
 import NoticeCategoryItem from "../NoticeCategoryItem/NoticeCategoryItem";
 import { BoxList, BoxButton, Button } from "./NoticesCategoriesList.styled";
@@ -9,13 +8,15 @@ import { BoxList, BoxButton, Button } from "./NoticesCategoriesList.styled";
 const NoticesCategoriesList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const dispatch = useDispatch();
-  // const user = useSelector(getUser);
   const filteredNotices = useSelector(filteredNoticesSelector);
 
   useEffect(() => {
-    dispatch(userNotice());
-  }, [dispatch]);
+    dispatch(userNotice(currentPage));
+  }, [dispatch, currentPage]);
 
+  if (!filteredNotices) {
+    return;
+  }
   const totalPage = filteredNotices.length / currentPage / 8;
 
   const nextPage = () => setCurrentPage((prev) => prev + 1);
@@ -34,7 +35,7 @@ const NoticesCategoriesList = () => {
             Prev Page
           </Button>
         )}
-        {totalPage !== currentPage && (
+        {totalPage !== currentPage && totalPage && (
           <Button type="submit" onClick={nextPage}>
             Next Page
           </Button>
